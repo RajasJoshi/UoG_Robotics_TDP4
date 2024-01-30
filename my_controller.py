8720b2c6711496969035dfe79dd2e0658e6cdf94
Sovik-Ghosh <157396226+Sovik-Ghosh@users.noreply.github.com>
2024-01-30 16:24:21 +0000

    Add files via upload

--- my_controller.py
+++ my_controller.py
@@ -1,9 +1,7 @@
-"""my_controller controller."""
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 import pip
 pip.main(['install', 'lapx>=0.5.2'])
+pip.main(['install', 'torch', 'torchaudio', 'torchvision', '--index-url', 'https://download.pytorch.org/whl/cu118'])
 
 from controller import Robot
 from collections import defaultdict
@@ -24,6 +22,8 @@ cameraTop.enable(timestep)
 track_history = defaultdict(lambda: [])
 track = []
 
+
+
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
 #  motor = robot.getDevice('motorname')
@@ -35,46 +35,53 @@ track = []
 while robot.step(timestep) != -1:
     image = cameraTop.getImage()
     width, height = cameraTop.getWidth(), cameraTop.getHeight()
-    
+   
     # Convert the image to a format usable by OpenCV
     image = np.frombuffer(image, np.uint8).reshape((height, width, 4))
 
     # Perform some OpenCV operations (e.g., grayscale conversion)
     frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
-    model = YOLO('/Users/sovikghosh/Desktop/best.pt')
+   
+    model = YOLO(r"F:\best.pt")
 
-    results = model.track(frame, persist=True)
+    results = model.predict(frame, device = 'cuda', classes = [0,1,2,3], conf = 0.5)
+    results1 = model.track(frame, persist = True, device = 'cuda', tracker = 'bytetrack.yaml', classes = [0], conf = 0.5)
 
     # Get the boxes and track IDs
-    boxes = results[0].boxes.xywh.cpu()
-    names = results[0].names
-    track_ids = results[0].boxes.id.int().cpu().tolist()
-    clss = results[0].boxes.cls.cpu().tolist()
+    boxes = results1[0].boxes.xywh.cpu().tolist()
+    names = results1[0].names
+    try:
+        track_ids = results1[0].boxes.id.cpu().tolist()
+    except:
+        track_ids = None
+    clss = results1[0].boxes.cls.cpu().tolist()
 
     # Visualize the results on the frame
     annotated_frame = results[0].plot()
 
     # Plot the tracks
-    for box, clss, track_id in zip(boxes, clss, track_ids):
-        x, y, w, h = box
-        if names[clss] == 'Ball':
+    if track_ids:
+        for box, track_id in zip(boxes, track_ids):
+            x, y, w, h = box
             track = track_history[track_id]
             track.append((float(x), float(y)))  # x, y center point
             if len(track) > 30:  # retain 90 tracks for 90 frames
                 track.pop(0)
 
-        # Draw the tracking lines
-        if track:
-            points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
-            cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
-        else: pass
+            # Draw the tracking lines
+            if track:
+                points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+                cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
+            else: continue
+    else: continue
 
     # Display the annotated frame
     cv2.namedWindow('Window', cv2.WINDOW_NORMAL)
 
     # Resize the window to your desired size
-    cv2.resizeWindow('Window', 400, 300) 
+    cv2.resizeWindow('Window', 400, 300)
+    
+    
     cv2.imshow("Window", annotated_frame)
 
     # Break the loop if 'q' is pressed
